4ee81dc4b5b848e3eb32a7f9d8fe95232cc4030e
ccattie <chris.cattie@gmail.com>
2016-10-11 19:29:38 -0400

    pdf

---
+++ script.js
@@ -0,0 +1,91 @@
+$('.blog').hide();
+
+$('#blog1li').click(function(){
+	$('#blog1').slideToggle();
+});
+
+
+
+// $('#blog2').hide();
+
+$('#blog2li').click(function(){
+	$('#blog2').slideToggle();
+});
+
+
+
+$('#blog3').hide();
+
+$('#blog3li').click(function(){
+	$('#blog3').slideToggle();
+});
+
+
+
+$('#blog4').hide();
+
+$('#blog4li').click(function(){
+	$('#blog4').slideToggle();
+});
+
+
+
+$('#blog5').hide();
+
+$('#blog5li').click(function(){
+	$('#blog5').slideToggle();
+});
+
+
+
+
+$('#blog6').hide();
+
+$('#blog16i').click(function(){
+	$('#blog6').slideToggle();
+});
+
+
+
+$('#blog7').hide();
+
+$('#blog7li').click(function(){
+	$('#blog7').slideToggle();
+});
+
+
+
+
+$('#blog8').hide();
+
+$('#blog8li').click(function(){
+	$('#blog8').slideToggle();
+});
+
+
+
+$('#blog1').hide();
+
+$('#blog1li').click(function(){
+	$('#blog1').slideToggle();
+});
+
+
+
+
+$('#blog1').hide();
+
+$('#blog1li').click(function(){
+	$('#blog1').slideToggle();
+});
+
+
+
+$('input').change(function(){
+	$('input').background-color('red');
+});
+
+$('#findaboardbutton').click(function(){
+	
+});
+
